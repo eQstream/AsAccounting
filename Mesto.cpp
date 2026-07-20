42444f1b79d8bb7ce25f086e6982e5f0cdddfb06
mindwork64 <vmurashov85@gmail.com>
2026-07-20 06:23:49 +0300

    .clang-format. Фрсим стиль Microsoft (в стиле LSL)

--- Mesto.cpp
+++ Mesto.cpp
@@ -3,56 +3,128 @@
 #include <string>
 
 // Конструктор, соответствующий .h
-Mesto::Mesto(int place_id, std::string first, std::string middle,
-             std::string last, std::string phone, std::string addr,
-             std::string pass, std::string model, std::string avto_num,
-             int balance, bool isRetired, bool isDiscount)
-    : id(place_id), first_name(first), middle_name(middle), last_name(last),
-      phone_number(phone), address(addr), passport(pass), avto_model(model),
-      avto_number(avto_num), spot_balance(balance), retired(isRetired),
-      discount(isDiscount) {}
+Mesto::Mesto(int place_id, std::string first, std::string middle, std::string last, std::string phone, std::string addr,
+             std::string pass, std::string model, std::string avto_num, int balance, bool isRetired, bool isDiscount)
+    : id(place_id), first_name(first), middle_name(middle), last_name(last), phone_number(phone), address(addr),
+      passport(pass), avto_model(model), avto_number(avto_num), spot_balance(balance), retired(isRetired),
+      discount(isDiscount)
+{
+}
 
-Mesto::~Mesto() {}
+Mesto::~Mesto()
+{
+}
 
 // Геттер просто возвращает значение
-int Mesto::getId() const { return id; }
-const std::string &Mesto::getFirstName() const { return first_name; }
-const std::string &Mesto::getMiddleName() const { return middle_name; }
-const std::string &Mesto::getLastName() const { return last_name; }
-const std::string &Mesto::getPhoneNumber() const { return phone_number; }
-const std::string &Mesto::getAddress() const { return address; }
-const std::string &Mesto::getPassport() const { return passport; }
-const std::string &Mesto::getAvtoModel() const { return avto_model; }
-const std::string &Mesto::getAvtoNumber() const { return avto_number; }
-int Mesto::getBaseMonthlyPayment() const { return (id <= 150) ? 1300 : 2000; }
-int Mesto::getFinalPayment() const {
-  return discount ? getBaseMonthlyPayment() * 0.8 : getBaseMonthlyPayment();
+int Mesto::getId() const
+{
+    return id;
 }
-int Mesto::getBalance() const { return spot_balance; }
-bool Mesto::getRetired() const { return retired; }
-bool Mesto::getDiscount() const { return discount; }
+const std::string &Mesto::getFirstName() const
+{
+    return first_name;
+}
+const std::string &Mesto::getMiddleName() const
+{
+    return middle_name;
+}
+const std::string &Mesto::getLastName() const
+{
+    return last_name;
+}
+const std::string &Mesto::getPhoneNumber() const
+{
+    return phone_number;
+}
+const std::string &Mesto::getAddress() const
+{
+    return address;
+}
+const std::string &Mesto::getPassport() const
+{
+    return passport;
+}
+const std::string &Mesto::getAvtoModel() const
+{
+    return avto_model;
+}
+const std::string &Mesto::getAvtoNumber() const
+{
+    return avto_number;
+}
+int Mesto::getBaseMonthlyPayment() const
+{
+    return (id <= 150) ? 1300 : 2000;
+}
+int Mesto::getFinalPayment() const
+{
+    return discount ? getBaseMonthlyPayment() * 0.8 : getBaseMonthlyPayment();
+}
+int Mesto::getBalance() const
+{
+    return spot_balance;
+}
+bool Mesto::getRetired() const
+{
+    return retired;
+}
+bool Mesto::getDiscount() const
+{
+    return discount;
+}
 
 // Сеттеры
-void Mesto::setBalance(const int balance) { spot_balance = balance; }
-
-void Mesto::setFirstName(const std::string &first) { first_name = first; }
-
-void Mesto::setMiddleName(const std::string &middle) { middle_name = middle; }
-
-void Mesto::setLastName(const std::string &last) { last_name = last; }
-
-void Mesto::setPhone(const std::string &phone) { phone_number = phone; }
-
-void Mesto::setAddress(const std::string &addr) { address = addr; }
-
-void Mesto::setPassport(const std::string &pass) { passport = pass; }
-
-void Mesto::setAvtoModel(const std::string &model) { avto_model = model; }
-
-void Mesto::setAvtoNumber(const std::string &avto_num) {
-  avto_number = avto_num;
+void Mesto::setBalance(const int balance)
+{
+    spot_balance = balance;
 }
 
-void Mesto::setRetired(const bool isRetired) { retired = isRetired; }
+void Mesto::setFirstName(const std::string &first)
+{
+    first_name = first;
+}
 
-void Mesto::setDiscount(bool isDiscount) { discount = isDiscount; }
+void Mesto::setMiddleName(const std::string &middle)
+{
+    middle_name = middle;
+}
+
+void Mesto::setLastName(const std::string &last)
+{
+    last_name = last;
+}
+
+void Mesto::setPhone(const std::string &phone)
+{
+    phone_number = phone;
+}
+
+void Mesto::setAddress(const std::string &addr)
+{
+    address = addr;
+}
+
+void Mesto::setPassport(const std::string &pass)
+{
+    passport = pass;
+}
+
+void Mesto::setAvtoModel(const std::string &model)
+{
+    avto_model = model;
+}
+
+void Mesto::setAvtoNumber(const std::string &avto_num)
+{
+    avto_number = avto_num;
+}
+
+void Mesto::setRetired(const bool isRetired)
+{
+    retired = isRetired;
+}
+
+void Mesto::setDiscount(bool isDiscount)
+{
+    discount = isDiscount;
+}
--- Mesto.h
+++ Mesto.h
@@ -1,51 +1,50 @@
 #pragma once
 #include <string>
 
-class Mesto {
+class Mesto
+{
 private:
-  int id; // Нмер места
-  std::string first_name;
-  std::string middle_name;
-  std::string last_name;
-  std::string phone_number;
-  std::string address;
-  std::string passport;
-  std::string avto_model;
-  std::string avto_number;
-  int spot_balance;
-  bool retired;
-  bool discount; // имеет ли право на льготы
+    int id; // Нмер места
+    std::string first_name;
+    std::string middle_name;
+    std::string last_name;
+    std::string phone_number;
+    std::string address;
+    std::string passport;
+    std::string avto_model;
+    std::string avto_number;
+    int spot_balance;
+    bool retired;
+    bool discount; // имеет ли право на льготы
 
 public:
-  Mesto(int place_id, std::string first = "", std::string middle = "",
-        std::string last = "", std::string phone = "", std::string addr = "",
-        std::string pass = "", std::string model = "",
-        std::string avto_num = "", int balance = 0, bool isRetired = false,
-        bool isDiscount = false);
-  ~Mesto();
-  int getId() const;
-  const std::string &getFirstName() const;
-  const std::string &getMiddleName() const;
-  const std::string &getLastName() const;
-  const std::string &getPhoneNumber() const;
-  const std::string &getAddress() const;
-  const std::string &getPassport() const;
-  const std::string &getAvtoModel() const;
-  const std::string &getAvtoNumber() const;
-  int getBaseMonthlyPayment() const;
-  int getFinalPayment() const;
-  int getBalance() const;
-  bool getRetired() const;
-  bool getDiscount() const;
-  void setFirstName(const std::string &first);
-  void setMiddleName(const std::string &middle);
-  void setLastName(const std::string &last);
-  void setPhone(const std::string &phone);
-  void setAddress(const std::string &addr);
-  void setPassport(const std::string &pass);
-  void setAvtoModel(const std::string &model);
-  void setAvtoNumber(const std::string &avto_num);
-  void setBalance(const int balance);
-  void setRetired(const bool isRetired);
-  void setDiscount(const bool discount);
+    Mesto(int place_id, std::string first = "", std::string middle = "", std::string last = "", std::string phone = "",
+          std::string addr = "", std::string pass = "", std::string model = "", std::string avto_num = "",
+          int balance = 0, bool isRetired = false, bool isDiscount = false);
+    ~Mesto();
+    int getId() const;
+    const std::string &getFirstName() const;
+    const std::string &getMiddleName() const;
+    const std::string &getLastName() const;
+    const std::string &getPhoneNumber() const;
+    const std::string &getAddress() const;
+    const std::string &getPassport() const;
+    const std::string &getAvtoModel() const;
+    const std::string &getAvtoNumber() const;
+    int getBaseMonthlyPayment() const;
+    int getFinalPayment() const;
+    int getBalance() const;
+    bool getRetired() const;
+    bool getDiscount() const;
+    void setFirstName(const std::string &first);
+    void setMiddleName(const std::string &middle);
+    void setLastName(const std::string &last);
+    void setPhone(const std::string &phone);
+    void setAddress(const std::string &addr);
+    void setPassport(const std::string &pass);
+    void setAvtoModel(const std::string &model);
+    void setAvtoNumber(const std::string &avto_num);
+    void setBalance(const int balance);
+    void setRetired(const bool isRetired);
+    void setDiscount(const bool discount);
 };
