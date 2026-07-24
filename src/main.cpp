ce1b2d4b08ae260e00fa8fcb953e2a3c7b20db8d
mindwork64 <vmurashov85@gmail.com>
2026-07-24 18:31:13 +0300

    Closes #17

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
-/// @file main.cpp
+// @file main.cpp
 /// @brief Точка входа в приложение "Гаражный учёт" (AsAccounting).
 
 #include "ui/MainWindow.h"
 #include <QApplication>
+
+#ifdef _WIN32
 #include <windows.h>
+#endif
 
 /// @brief Инициализирует Qt-приложение, настраивает кодировку консоли
 ///        и запускает главное окно.
@@ -14,8 +17,10 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
+#ifdef _WIN32
     // Установка UTF-8 для вывода в консоль Windows
     SetConsoleOutputCP(65001);
+#endif
 
     MainWindow window;
     window.show();
